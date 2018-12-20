602980fcd2353062922c86165118926e9648f15b
NRGLine4Sec <mailforgithub.nrgline@yopmail.com>
2018-12-20 20:33:34 +0100

    Update install_nfsen.sh

--- install_nfsen.sh
+++ install_nfsen.sh
@@ -1,7 +1,21 @@
 apt-get update && apt-get upgrade -y
 apt-get install php7.0 apache2 libapache2-mod-php7.0 php.10-common libmailtools-perl rrdtool librrds-perl nfdump nfdump-flow-tools perls6 -y
 cd /usr/local/src/
-wget https://downloads.sourceforge.net/project/nfsen/stable/nfsen-1.3.8/nfsen-1.3.8.tar.gz
+wget --no-check-certificate https://downloads.sourceforge.net/project/nfsen/stable/nfsen-1.3.8/nfsen-1.3.8.tar.gz
 tar -xf nfsen-1.3.8.tar.gz
 cd nfsen-1.3.8
-cp /etc/nfsen-dist.conf /et/nfsen.conf
+cp etc/nfsen-dist.conf etc/nfsen.conf
+useradd -m nfsen
+passwd nfsen
+groupadd nfsen
+usermod -G nfsen nfsen
+groupadd nfsenadmin
+usermod -a -G nfsenadmin nfsen
+usermod -a -G nfsenadmin www-data
+mkdir -p /var/www/html/nfsen
+chown -R nfsen:nfsenadmin /var/www/html/nfsen
+
+echo '$USER = "www-data";
+$WWWUSER = "www-data";
+$WWWGROUP = "www-data";' > /etc/nfsen.conf
+echo "%sources = ( 'ROUTER1' => { 'port' => '3333', 'col' => '#0000ff', 'type' => 'netflow' }, );" >> /etc/nfsen.conf
